5edb6b1c7aae277dbf3368233bce596077213621
Mi-Str <112564483+Mi-Str@users.noreply.github.com>
2022-10-23 17:58:37 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,41 @@
-# Makes the website folder a Python package
-# When we import the folder, everything that is inside the package will run automatically
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .db_init import get_db_connection
+
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
 
-def create app():
-    pass
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'This is my very secret key'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://newuser:password@localhost/website"
+    db.init_app(app)
 
-def create database_app():
-    pass
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .create_db import User, Note
+
+    create_database(app)
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+def create_database(app):
+    get_db_connection()
+    print('The database has been successfully created')

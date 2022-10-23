c482cc2577d4b82de725088def41e93356e73e05
Mi-Str <112564483+Mi-Str@users.noreply.github.com>
2022-10-23 18:00:28 +0100

    Update create_db.py

--- website/create_db.py
+++ website/create_db.py
@@ -1,4 +1,29 @@
 # We are using this file to sort out the database
 
 from . import db
- Class Users()
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Note(db.Model,Base):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin, Base):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+class UserEvent(db.Model,Base):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))

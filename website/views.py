69bd6479feebf31734a16596ce075d5a83550db4
NenehPatel <112558752+NenehPatel@users.noreply.github.com>
2022-11-05 19:41:38 +0000

    Updated event search routes

--- website/views.py
+++ website/views.py
@@ -2,12 +2,13 @@ from flask import Blueprint, request, render_template, flash, url_for, redirect
 from flask_login import login_required, current_user
 from .create_db import Note, User
 from . import db
-from .api.events import get_events, get_event_by_id, search
+from .api.combined_events import CombinedEventManager
 from .create_note import create_note, add_event
 
 
-
 views = Blueprint('views', __name__)
+
+combined_event_manager = CombinedEventManager()
 
 
 @views.route('/')
@@ -17,20 +18,20 @@ def home():
 
 @views.route('/events')
 def events():
-    event_details = get_events()
+    event_details = combined_event_manager.get_events()
     return render_template("events.html", user=current_user, event_details=event_details)
 
 
 @views.route('/event/<event_id>')
 def event(event_id):
-    event = get_event_by_id(event_id)
+    event = combined_event_manager.get_event_by_id(event_id)
     return render_template("event.html", user=current_user, event=event)
 
 
 @views.route('/search')
-def search_events():
+def event_search():
     query = request.args.get('query')
-    event_data = search(query) if query else None
+    event_data = combined_event_manager.get_events(query) if query else None
     return render_template("search.html", user=current_user, event_data=event_data)
 
 
@@ -63,6 +64,3 @@ def add_note():
 def add_event():
     event = add_event
     return render_template("events.html", user=current_user, event=event)
-
-
-
